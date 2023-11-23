1ede0427971a66f0b36a0c5dd1172296df76f996
Musab Nasredlin <musabagab2010@gmail.com>
2023-11-23 10:17:36 +0400

    Downgrade JVM Target version

--- source-code/final-project/DownloadManager/app/build.gradle.kts
+++ source-code/final-project/DownloadManager/app/build.gradle.kts
@@ -34,11 +34,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_18
-        targetCompatibility = JavaVersion.VERSION_18
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "18"
+        jvmTarget = "1.8"
     }
     buildFeatures {
         compose = true
